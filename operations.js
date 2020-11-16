75aa29377e17864cd9a75dc80174ea7dc62ba176
Priya raja <priya.thevar89@gmail.com>
2020-11-16 13:01:50 +0400

    operations on a string

---
+++ operations.js
@@ -0,0 +1,7 @@
+let fruits = 'banana,apple,orange';
+console.log(fruits.split('')); 
+console.log(fruits.indexOf('nan')); //find the index starting with nan
+console.log(fruits.replace('ban',123));  //replace the word in banana
+console.log(fruits.split(',')); //split after every word ,
+console.log(fruits.charAt(2));
+console.log(fruits.slice(2,5));
