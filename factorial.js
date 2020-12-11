3840ab88d828f0d2e3ca0e173bf4f205523d1206
Priya raja <priya.thevar89@gmail.com>
2020-12-11 09:55:00 +0400

    included a  digit function

--- factorial.js
+++ factorial.js
@@ -9,3 +9,15 @@ function factorialNumber(num) {
     return sum;
 }
 console.log(factorialNumber(5))
+
+function findDigit(str) {
+    const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    const digit = str.split('');
+    for(const di of digit){
+
+        if(numbers.includes(di)) {
+            return di;
+        }
+    }
+}
+console.log(findDigit('a2bhf'));
