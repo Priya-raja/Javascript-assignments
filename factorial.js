a92c91b8408620ef99fd2e06c9f0f2338ba1014f
Priya raja <priya.thevar89@gmail.com>
2020-12-11 09:19:20 +0400

    factorial in js

---
+++ factorial.js
@@ -0,0 +1,11 @@
+function factorialNumber(num) {
+    
+    // write code here
+    let sum = 1;
+    for(let i=1; i<=num; i++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
+console.log(factorialNumber(5))
